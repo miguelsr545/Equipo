14738dd0d013fe6f276218b55f54bb2da1de4810
Miguel Sanchez <miguelsr434@gmail.com>
2020-09-29 18:24:17 -0500

    Bitwise Miguel
    Para que el profe lo cheque

--- corrimiento.c
+++ corrimiento.c
@@ -1,39 +1,53 @@
 #include <stdio.h>
 
-void main ( void ) {
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
 
-    char NumeroBits, OperacionVar, Corrimiento, Resultado ;
-    printf( "Ingrese el numero de bits" );
-    scanf( "%d", &NumeroBits );
-
-    if ( NumeroBits == 8 || 16 || 32 ) {
-        printf("1) And \n2) OR \n3) XOR");
-        scanf( "%d", &OperacionVar );
-
-        printf( "Ingrese el corrimiento" );
-        scanf( "%d", &Corrimiento );
-
-        if ( NumeroBits < Corrimiento && OperacionVar >=1 || OperacionVar <=3) {
-        switch ( OperacionVar ) {
-
-            case 1 :
-                Resultado &=~ ( 1<< Corrimiento );
-            break;
-
-            case 2:
-                Resultado |= (1<< Corrimiento);
-            break;
-
-            case 3:
-                Resultado^= (1 << Corrimiento);
-            break;
-        } printf("El resultado es: %d", Resultado)
-    }
+int main()
+{
+    uint32 u32NumeroBits = 0;
+    uint32 u32OperacionVar = 0;
+    uint32 u32Corrimiento = 0;
+    uint32 u32Resultado = 0;
+    while ( 1 ) {
+    printf("Ingrese el numero de bits\n");
+    scanf("%d", &u32NumeroBits);
     
+    if (u32NumeroBits == 8 || u32NumeroBits == 16 || u32NumeroBits == 32) {
+
+        printf("1) And \n2) OR \n3) XOR\n");
+        scanf("%d", &u32OperacionVar);
+        if ( u32OperacionVar >= 1 && u32OperacionVar <= 3 ) {
+
+            printf("Ingrese el corrimiento\n");
+            scanf("%d", &u32Corrimiento);
+
+            if ( u32OperacionVar == 1 ) {
+
+                u32Resultado &=~ ( 1 << u32Corrimiento );
+
+            }
+            else if (u32OperacionVar == 2) {
+
+                u32Resultado |= (1 << u32Corrimiento);
+
+            }
+            else {
+
+                u32Resultado ^= (1 << u32Corrimiento);
+
+            }
+
+            printf("\nResultado %d \n", u32Resultado);
+        } else {
+            printf("El numero debe de ser entre 1 y 3\n\n");
+        }
+    } else {
+        
+        printf("El tamanno debe de ser 8, 16 o 32\n\n");
+
     }
-    else 
-
-    
-
-
+    }
+    return 0;
 }
