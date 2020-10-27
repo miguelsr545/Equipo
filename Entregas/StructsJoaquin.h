14063cfd38b1e583ec02923cc8f29d964514e115
Joaquin Montiel <joaquin.montiel@edu.uag.mx>
2020-10-27 17:21:18 -0600

    3.0

--- Entregas/StructsJoaquin.h
+++ Entregas/StructsJoaquin.h
@@ -21,10 +21,10 @@ typedef enum
 #define SECTOR4_SLOTS		12
 
 
-void ShowAvailableSlots (uint8 pu8Sector);
-void ShowNotAvailableSlots (uint8 pu8Sector, uint8 Sector);
-void ShowEarnedMoney ( uint8 pu8Sector );
-uint8 u8AddCarToSector( uint8 pu8Sector, uint8 u8check );
-uint8 u8RemoveCarFromSector( uint8 pu8Sector, uint8 u8check, uint8 pu8ParkingFee );
-void u8ImportPaymentOfSlot( uint8 pu8Tot, uint8 pu8Add );
+void ShowAvailableSlots (uint8 *pu8Sector);
+void ShowNotAvailableSlots (uint8 *pu8Sector, uint8 Sector);
+void ShowEarnedMoney ( uint8 *pu8Sector );
+uint8 u8AddCarToSector( uint8 *pu8Sector, uint8 u8check );
+uint8 u8RemoveCarFromSector( uint8 *pu8Sector, uint8 u8check, uint8 *pu8ParkingFee );
+void u8ImportPaymentOfSlot( uint8 *pu8Tot, uint8 *pu8Add );
 uint8 printRandoms(uint8 0, uint8 2,  uint8 count);
