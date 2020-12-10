091ee969b71fb33a02290130dce532c06c1ab334
Miguel Sanchez <miguelsr434@gmail.com>
2020-12-10 10:54:49 -0600

    Estacionamiento V1.2
    .

--- Practicas/Proyecto/proyecto.c
+++ Practicas/Proyecto/proyecto.c
@@ -1,73 +1,125 @@
 #include "types.h"
 
-tstMax_X_COORDS Park[SIZE_PARKING] = TABLERO;
+tstPark Park[SIZE_PARKING] = ESTACIONAMIENTO;
 tstTime Time_Slot[SIZE_PARKING] = TABLERO_TIEMPO;
+static uint8 u8States_Estacionamiento = Estacionamiento_Abierto;
+uint8 u8Hora = ABRIR;
 
 void main ( void )
 {   
-    FuncPark();
-}
-
-void FuncPark ( void )
-{   
     while ( TRUE )
-    {
-        uint8 u8Accion_Park;
-        printf("Bienvenido al estacionamiento\n");
-        printf("1) Ingresar auto\n");
-        printf("2) Sacar auto\n");
-        printf("3) Ver espacios Disponibles\n");
-        printf("4) Pasar 1 hora");
-        scanf("%d", &u8Accion_Park);
-
-        if ( u8Accion_Park == Meter )
+    {   
+        if ( u8States_Estacionamiento == Estacionamiento_Abierto )
         {
-            MeterCoche ( );
+            FuncParkOpen();
         }
-        else if ( u8Accion_Park == Sacar )
+        else if ( u8States_Estacionamiento == Estacionamiento_Cerrado )
         {
-            SacarCoche ( );
-        }
-        else if ( u8Accion_Park == VerDisponibles )
-        {
-            MostrarEspacios ( );
-        }
-        else if ( u8Accion_Park == PasarHora )
-        {
-            TimeSlot ( );
-        }
-        
-        else 
-        {
-            printf("Accion invalida");
-        }
+            FuncParkClose();
+        }   
     }
 }
 
+void FuncParkOpen ( void )
+{     
+    uint8 u8Accion_Park;
+    uint8 u8Seguro;
+    printf("\n\n| %d:00 |\n", u8Hora);
+    printf("Bienvenido al estacionamiento\n");
+    printf("1) Ingresar auto\n");
+    printf("2) Sacar auto\n");
+    printf("3) Ver espacios Disponibles\n");
+    printf("4) Pasar 1 hora");
+    scanf("%d", &u8Accion_Park);
+    if ( u8Accion_Park == Meter )
+    {
+        MeterCoche ( );
+    }
+    else if ( u8Accion_Park == Sacar )
+    {
+        SacarCoche ( );
+    }
+    else if ( u8Accion_Park == VerDisponibles )
+    {
+        MostrarEspacios ( );
+    }
+    else if ( u8Accion_Park == PasarHora )
+    {
+        TimeSlot ( );
+    }
+    else 
+    {
+        printf("\nAccion invalida presione una tecla para continuar\n");
+        scanf("%c", &u8Seguro);
+    }
+}
+
+void FuncParkClose( void )
+{   
+    uint8 u8Accion_Park;
+    uint8 u8Seguro;
+    printf("\n\n| %d:00 |\n", u8Hora);
+    printf("El estacionamiento esta cerrado abrira a las %d\n", ABRIR);
+    printf("1) Ingresar auto\n");
+    printf("2) Sacar auto\n");
+    printf("3) Ver espacios Disponibles\n");
+    printf("4) Pasar 1 hora");
+    scanf("%d", &u8Accion_Park);
+    
+    if ( u8Accion_Park == Meter )
+    {
+        MeterCoche ( );
+    }
+    else if ( u8Accion_Park == Sacar )
+    {
+        SacarCoche ( );
+    }
+    else if ( u8Accion_Park == VerDisponibles )
+    {
+        MostrarEspacios ( );
+    }
+    else if ( u8Accion_Park == PasarHora )
+    {
+        TimeSlot ( );
+    }
+    else 
+    {
+        printf("\nAccion invalida presione una tecla para continuar\n");
+        scanf("%c", &u8Seguro);
+    }
+}
 void MeterCoche ( void )
 {   
     uint8 u8X;
     uint8 u8Y;
     uint8 u8Seguro = TRUE;
-    while ( u8Seguro == TRUE )
+    if ( u8States_Estacionamiento == Estacionamiento_Abierto )
     {
-        srand(time(NULL));
-        u8X = printRandoms(LIMITE_INF,LIMITE_SUP,UNO);
-        u8Y = printRandoms(LIMITE_INF,LIMITE_SUP,UNO);
+        while ( u8Seguro == TRUE )
+        {
+            srand(time(CERO));
+            u8X = printRandoms(LIMITE_INF,LIMITE_SUP,UNO);
+            u8Y = printRandoms(LIMITE_INF,LIMITE_SUP,UNO);
 
-        if ( Park[u8Y].u8Cordenadas_X[u8X] == COCHE )
-        {
-            //Nada
+            if ( Park[u8Y].u8Cordenadas_X[u8X] == COCHE )
+            {
+                //Nada
+            }
+            else 
+            {
+                Park[u8Y].u8Cordenadas_X[u8X] = COCHE;
+                u8Seguro = FALSE;
+            }
+
         }
-        else 
-        {
-            Park[u8Y].u8Cordenadas_X[u8X] = COCHE;
-            u8Seguro = FALSE;
-        }
-        
+        printf("Se estaciono en %d,%d\n", u8X, u8Y);
+        Time_Slot[u8Y].u8Tiempo[u8X] = CERO;
     }
-    printf("Se estaciono en %d,%d\n", u8X, u8Y);
-    Time_Slot[u8Y].u8Tiempo[u8X] = NULL;
+    else if ( u8States_Estacionamiento == Estacionamiento_Cerrado )
+    {
+        printf("\nNo es posible ingresar auto cuando el estacionamiento esta cerrado\n");
+    }
+    
 }
 
 void SacarCoche ( void )
@@ -83,25 +135,38 @@ void SacarCoche ( void )
     printf("En que posicion de X\n");
     scanf("%d", &u8XCoord);
     //printf("Y=%d\n", u8YCoord);
-    Park[u8YCoord].u8Cordenadas_X[u8XCoord] = CASILLAS;
-    //printf("x=%d\n", u8XCoord);
-    //printf("Y=%d\n", u8YCoord);
-    u16Cobro = Time_Slot[u8YCoord].u8Tiempo[u8XCoord]*PRECIO;
-    printf("Debe de pagar $%d\n", u16Cobro);
+
+    if ( Park[u8YCoord].u8Cordenadas_X[u8XCoord] == COCHE )
+    {
+        Park[u8YCoord].u8Cordenadas_X[u8XCoord] = CASILLAS;
+        //printf("x=%d\n", u8XCoord);
+        //printf("Y=%d\n", u8YCoord);
+        u16Cobro = Time_Slot[u8YCoord].u8Tiempo[u8XCoord]*PRECIO;
+        printf("Debe de pagar $%d\n", u16Cobro);
+    }
+    else
+    {
+        printf("Este espacio esta libre");
+    }
+    
+
 }
 
 void MostrarEspacios ( void )
 {   
+    
     uint8 u8j;
     uint8 u8i;
-    for ( u8j = 0 ; u8j < SIZE_PARKING ; u8j++)
+    for ( u8j = CERO ; u8j < SIZE_PARKING ; u8j++)
     {   
-        for ( u8i = 0 ; u8i < SIZE_PARKING ; u8i++ )
+        for ( u8i = CERO ; u8i < SIZE_PARKING ; u8i++ )
         {   
             printf("%c ", Park[u8j].u8Cordenadas_X[u8i]);
         }
         printf("\n");
     }
+    
+    
 }
 
 uint8 printRandoms(uint8 lower, uint8 upper,  uint8 count) 
@@ -116,13 +181,34 @@ void TimeSlot ( void )
 {   
     uint8 u8j;
     uint8 u8i;
-    for ( u8j = 1 ; u8j < 16 ; u8j++ )
+    for ( u8j = UNO ; u8j < LIMIT_SLOT ; u8j++ )
     {
-        for ( u8i = 1 ; u8i < 16 ; u8i++ )
+        for ( u8i = UNO ; u8i < LIMIT_SLOT ; u8i++ )
         {
             Time_Slot[u8j].u8Tiempo[u8i]++;
         }
     }
+
+    u8Hora++;
+    if ( u8Hora == FIN_DIA)
+    {
+        u8Hora = CERO;
+    }
+    else
+    {
+        /*Sigue conteo normal*/
+    }
+    
+    if ( u8Hora >= ABRIR && u8Hora < CERRAR )
+    {
+        u8States_Estacionamiento = Estacionamiento_Abierto;
+    }
+    else 
+    {
+        u8States_Estacionamiento = Estacionamiento_Cerrado;
+    }
+
+    
     // for ( u8j = 0 ; u8j < SIZE_PARKING ; u8j++)
     // {   
     //     for ( u8i = 0 ; u8i < SIZE_PARKING ; u8i++ )
--- Practicas/Proyecto/types.h
+++ Practicas/Proyecto/types.h
@@ -16,19 +16,24 @@ typedef unsigned int uint32;
 
 #define LIMITE_INF          1
 #define LIMITE_SUP          15
+#define LIMIT_SLOT          16
 
 #define UNO                 1
-#define COCHE               88
+#define CERO                0
+#define COCHE               184
 #define PRECIO              20
 
 #define TRUE                1
 #define FALSE               0
+#define ABRIR               7
+#define CERRAR              21
+#define FIN_DIA             24
 
 typedef struct 
 {
     uint8 u8Cordenadas_X[SIZE_PARKING];
 
-}tstMax_X_COORDS;
+}tstPark;
 
 
 typedef struct 
@@ -43,9 +48,15 @@ typedef enum
     Sacar,
     VerDisponibles,
     PasarHora
-}enmAcciones;
+}enAcciones;
 
-#define TABLERO     \
+typedef enum
+{
+    Estacionamiento_Abierto = 1,
+    Estacionamiento_Cerrado
+}enEstados;
+
+#define ESTACIONAMIENTO     \
 {					\
     {E_SUP_IZQ,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,BORDE_HORIZONTAL,E_SUP_DER},             \
     {BORDE_LATERAL,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,CASILLAS,BORDE_LATERAL},                                                                                                                             \
@@ -68,27 +79,28 @@ typedef enum
 
 #define TABLERO_TIEMPO     \
 {					\
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},                                                                                                                             \
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},                                                                                                                             \
+    {CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO,CERO},             \
 }
 
 
-void FuncPark( void );
+void FuncParkOpen ( void );
+void FuncParkClose( void );
 void MeterCoche ( void );
 void SacarCoche ( void );
 void MostrarEspacios ( void );
